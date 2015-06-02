51b26290f5336b9ec95ac9164aa9b3ec97223b7f
mrathbun <mrathbun@buffalo.edu>
2015-06-02 19:14:37 -0400

    Major indentation fix for main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def send_data(msg):
             if sent == 0:
                 raise RuntimeError("socket connection broken")
             totalsent = totalsent + sent
-	#print 'sent message ' + msg
+    #print 'sent message ' + msg
     except Exception, e:
         if roverSocket is None:
             return
@@ -56,14 +56,14 @@ def send_data(msg):
         roverSocket = None
 
 def ping(self, hostname):
-	if self.client is None or self.addr is None:
-		print "No client registered"
-		return
-	response = os.system("ping -c 1 " + addr)
-	if response == 0:
-		return True
-	else:
-		return False
+    if self.client is None or self.addr is None:
+        print "No client registered"
+        return
+    response = os.system("ping -c 1 " + addr)
+    if response == 0:
+        return True
+    else:
+        return False
 
 class Rover(QtGui.QMainWindow):
 
@@ -75,50 +75,51 @@ class Rover(QtGui.QMainWindow):
         self.editText = MyTextEdit(self)
         self.setCentralWidget(self.editText)
         self.initUI()
-	self.xbox = None
-	self.server = None
+        self.xbox = None
+        self.server = None
         self.startServer()
         self.startXbox()
         self.camValue = 0
         self.FPS = 15
+        self.blobUpdateRate = 2
         
     def initUI(self):   
 
-	#Labels for the xbox values# shoulderSend,elbowSend,baseSend,manipulatorSend,clawState,rightMotorSend,leftMotorSend
+    #Labels for the xbox values# shoulderSend,elbowSend,baseSend,manipulatorSend,clawState,rightMotorSend,leftMotorSend
         titleFont = QtGui.QFont('Times', 16, QtGui.QFont.Bold)        
 
         self.controlLabel=QtGui.QLabel('Xbox Control Values',self)
         self.controlLabel.setGeometry(10, 650, 300, 30)
         self.controlLabel.setStyleSheet("background:#0079E4; color:white")
         self.controlLabel.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
-	self.controlLabel.setFont(titleFont)
+        self.controlLabel.setFont(titleFont)
         self.xboxLabelShoulder=QtGui.QLabel('',self)
-	self.xboxLabelShoulder.setGeometry(10, 690, 300, 30)
-	self.xboxLabelShoulder.setStyleSheet("background:white;color:black;")
+        self.xboxLabelShoulder.setGeometry(10, 690, 300, 30)
+        self.xboxLabelShoulder.setStyleSheet("background:white;color:black;")
         self.xboxLabelShoulder.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
-	self.xboxLabelElbow=QtGui.QLabel('',self)
-	self.xboxLabelElbow.setGeometry(10, 730, 300, 30)
-	self.xboxLabelElbow.setStyleSheet("background:white;color:black;")
+        self.xboxLabelElbow=QtGui.QLabel('',self)
+        self.xboxLabelElbow.setGeometry(10, 730, 300, 30)
+        self.xboxLabelElbow.setStyleSheet("background:white;color:black;")
         self.xboxLabelElbow.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
-	self.xboxLabelBase=QtGui.QLabel('',self)
-	self.xboxLabelBase.setGeometry(10, 770, 300, 30)
-	self.xboxLabelBase.setStyleSheet("background:white;color:black;")
+        self.xboxLabelBase=QtGui.QLabel('',self)
+        self.xboxLabelBase.setGeometry(10, 770, 300, 30)
+        self.xboxLabelBase.setStyleSheet("background:white;color:black;")
         self.xboxLabelBase.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
-	self.xboxLabelManipulator=QtGui.QLabel('',self)
-	self.xboxLabelManipulator.setGeometry(10, 810, 300, 30)
-	self.xboxLabelManipulator.setStyleSheet("background:white;color:black;")
+        self.xboxLabelManipulator=QtGui.QLabel('',self)
+        self.xboxLabelManipulator.setGeometry(10, 810, 300, 30)
+        self.xboxLabelManipulator.setStyleSheet("background:white;color:black;")
         self.xboxLabelManipulator.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
-	self.xboxLabelClawState=QtGui.QLabel('',self)
-	self.xboxLabelClawState.setGeometry(10, 850, 300, 30)
-	self.xboxLabelClawState.setStyleSheet("background:white;color:black;")
+        self.xboxLabelClawState=QtGui.QLabel('',self)
+        self.xboxLabelClawState.setGeometry(10, 850, 300, 30)
+        self.xboxLabelClawState.setStyleSheet("background:white;color:black;")
         self.xboxLabelClawState.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
-	self.xboxLabelRightMotor=QtGui.QLabel('',self)
-	self.xboxLabelRightMotor.setGeometry(10, 890, 300, 30)
-	self.xboxLabelRightMotor.setStyleSheet("background:white;color:black;")
+        self.xboxLabelRightMotor=QtGui.QLabel('',self)
+        self.xboxLabelRightMotor.setGeometry(10, 890, 300, 30)
+        self.xboxLabelRightMotor.setStyleSheet("background:white;color:black;")
         self.xboxLabelRightMotor.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
-	self.xboxLabelLeftMotor=QtGui.QLabel('',self)
-	self.xboxLabelLeftMotor.setGeometry(10, 930, 300, 30)
-	self.xboxLabelLeftMotor.setStyleSheet("background:white;color:black;")
+        self.xboxLabelLeftMotor=QtGui.QLabel('',self)
+        self.xboxLabelLeftMotor.setGeometry(10, 930, 300, 30)
+        self.xboxLabelLeftMotor.setStyleSheet("background:white;color:black;")
         self.xboxLabelLeftMotor.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
 
         self.sensorLabel=QtGui.QLabel('Rover Sensor Values',self)
@@ -127,12 +128,12 @@ class Rover(QtGui.QMainWindow):
         self.sensorLabel.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
         self.sensorLabel.setFont(titleFont)
 
-	#Label for the temperature
-	self.metaInformation=QtGui.QLabel('Meta Information',self)
-	self.metaInformation.setGeometry(910, 690, 300, 30)
-	self.metaInformation.setStyleSheet("background:white;color:black;")
+        #Label for the temperature
+        self.metaInformation=QtGui.QLabel('Meta Information',self)
+        self.metaInformation.setGeometry(910, 690, 300, 30)
+        self.metaInformation.setStyleSheet("background:white;color:black;")
         self.metaInformation.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
-	
+    
         self.motorcurrent1=QtGui.QLabel('Motor Current 1',self)
         self.motorcurrent1.setGeometry(910, 730, 300, 30)
         self.motorcurrent1.setStyleSheet("background:white;color:black;")
@@ -163,10 +164,10 @@ class Rover(QtGui.QMainWindow):
         self.roll.setStyleSheet("background:white;color:black;")
         self.roll.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
 
-#        self.angle=QtGui.QLabel('Angle',self)
-#        self.angle.setGeometry(910, 970, 300, 30)
-#        self.angle.setStyleSheet("background:white;color:black;")
-#        self.angle.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
+        #self.angle=QtGui.QLabel('Angle',self)
+        #self.angle.setGeometry(910, 970, 300, 30)
+        #self.angle.setStyleSheet("background:white;color:black;")
+        #self.angle.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
 
         #self.roverTemp=QtGui.QLabel('Rover Temp: ',self)
         #self.roverTemp.setGeometry(910, 1010, 300, 30)
@@ -190,10 +191,28 @@ class Rover(QtGui.QMainWindow):
         comboCameraSelect.addItem("camera3")
         comboCameraSelect.setStyleSheet("background:white;color:black;")
 
-
         comboCameraSelect.activated[str].connect(self.onComboCamSelected)
         self.comboCameraSelect = comboCameraSelect
-        
+
+
+        #Blob Rate Select Label 
+        self.blobLabel=QtGui.QLabel('Blob Rate',self)
+        self.blobLabel.setGeometry(505, 930, 90, 30)
+        self.blobLabel.setStyleSheet("background:#0079E4; color:white")
+        self.blobLabel.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
+        self.blobLabel.setFont(titleFont)
+
+        #Blob Rate Select ComboBox
+        comboBlobUpdateRateSelect = QtGui.QComboBox(self)
+        comboBlobUpdateRateSelect.setGeometry(505, 970, 90, 60)
+        for i in range(1, 11):
+            comboBlobUpdateRateSelect.addItem('%d' % (i))
+        comboBlobUpdateRateSelect.setStyleSheet("background:white;color:black;")
+
+        comboBlobUpdateRateSelect.activated[str].connect(self.onComboBlobUpdateRateSelected)
+        self.comboBlobUpdateRateSelect = comboBlobUpdateRateSelect
+
+
         #FPS Label       
         self.FPSLabel=QtGui.QLabel('FPS',self)
         self.FPSLabel.setGeometry(625, 830, 90, 30)
@@ -216,14 +235,14 @@ class Rover(QtGui.QMainWindow):
         comboFPSSelect.activated[str].connect(self.onComboFPSSelected)
         self.comboFPSSelect = comboFPSSelect
 
-	#The main Video widget	
-	pic = QtGui.QWidget(self)
+        #The main Video widget    
+        pic = QtGui.QWidget(self)
         pic.setGeometry(10, 10, 1200, 620)
         pic.setStyleSheet("image:url(/home/sblinux/Pictures/Bulls.jpg);background:white;background-repeat:repat; ")
         pic.setAttribute(0,1);
         pic.setAttribute(3,1);
 
-	#Start button for the camera feed      
+        #Start button for the camera feed      
         startButton = QtGui.QPushButton("Start Cam",self)
         startButton.clicked.connect(self.startCam)
         startButton.setGeometry(505,690,90,60)       
@@ -262,31 +281,38 @@ class Rover(QtGui.QMainWindow):
         self.pic = pic
         self.center()
         self.showMaximized()
-	#self.resize(825, 600)
+        #self.resize(825, 600)
         self.setWindowIcon(QtGui.QIcon('/home/akash/Downloads/bulls1.jpg')) 
         self.setWindowTitle('Ub - Rasc-Al')
         self.show()
         self.cam = None
-	self.timer = QtCore.QTimer()
+        self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.resetStream)
         self.timer.start(20000)
 
                                
     def resetStream(self):
         if(self.cam != None): # and self.cam.isRunning()):
-            print("DFGvsdfCS")
             self.cam.quit()
             self.cam.start()
  
     def onComboCamSelected(self):
         self.camValue = self.comboCameraSelect.currentIndex()     
-        camstr = 'C' + str(self.camValue) + ',' + str(self.FPS) + ',480,640'
-	print camstr
-	send_data(camstr) 
+        camstr = 'C' + str(self.camValue) + ',' + str(self.FPS) + ',480,640,' + str(self.blobUpdateRate)
+        print camstr
+        send_data(camstr)
+        if self.cam:
+            self.cam.quit()
+
+    def onComboBlobUpdateRateSelected(self):
+        self.blobUpdateRate = self.comboBlobUpdateRateSelect.currentIndex() + 1
+        camstr = 'C' + str(self.camValue) + ',' + str(self.FPS) + ',480,640,' + str(self.blobUpdateRate)
+        print camstr
+        send_data(camstr) 
 
     def onComboFPSSelected(self):
         self.FPS = int(self.comboFPSSelect.currentText())
-	camstr = 'C' + str(self.camValue) + ',' + str(self.FPS) + ',480,640'
+        camstr = 'C' + str(self.camValue) + ',' + str(self.FPS) + ',480,640,' + str(self.blobUpdateRate)
         print camstr
         send_data(camstr) 
 
@@ -349,74 +375,71 @@ class Rover(QtGui.QMainWindow):
             self.cam = None
                 
     def startCam(self): 
-	try:      
-	        if(self.cam==None):   
-                    self.cam = camThread(int(self.pic.winId())) 
-                    self.cam.start()                
-	        elif(self.cam.isRunning()):            
-        	    pass
-	except Exception, e:
-		print e
+        try:      
+            if(self.cam==None):   
+                self.cam = camThread(int(self.pic.winId())) 
+                self.cam.start()                
+            elif(self.cam.isRunning()):            
+                pass
+        except Exception, e:
+            print e
         
     def startXbox(self):
-	         
-	if(self.xbox==None):  
-            self.xbox = xbox() 	
-	    self.connect( self.xbox,  self.xbox.signal, self.xboxCallBackfunc)    
-	    self.xbox.start() 
-	 
+        if(self.xbox==None):  
+            self.xbox = xbox()     
+            self.connect( self.xbox,  self.xbox.signal, self.xboxCallBackfunc)    
+            self.xbox.start() 
         else:
             self.xbox.stop() 
-	      
+          
     def xboxCallBackfunc(self,sigStr):
         if sigStr[0] is 'C':
             self.camValue = int(sigStr[1])
             self.comboCameraSelect.setCurrentIndex(self.camValue)
             sigStr = 'C' + str(self.camValue) + ',' + str(self.FPS) + ',480,640'
         elif sigStr[0] is 'l':
-	    signalArray = sigStr.split(',')
-	    self.xboxLabelShoulder.setText("Shoulder: "+signalArray[1])
-	    self.xboxLabelElbow.setText("Elbow: "+signalArray[0][1:])
-	    self.xboxLabelBase.setText("Base: "+signalArray[2])
-    	    self.xboxLabelManipulator.setText("Manipulator: "+signalArray[3])
-	    self.xboxLabelClawState.setText("ClawState: "+signalArray[4])
-	    self.xboxLabelRightMotor.setText("RightMotor: "+signalArray[5])
-	    self.xboxLabelLeftMotor.setText("LeftMotor: "+signalArray[6])	
-        send_data(sigStr)	
+            signalArray = sigStr.split(',')
+            self.xboxLabelShoulder.setText("Shoulder: "+signalArray[1])
+            self.xboxLabelElbow.setText("Elbow: "+signalArray[0][1:])
+            self.xboxLabelBase.setText("Base: "+signalArray[2])
+            self.xboxLabelManipulator.setText("Manipulator: "+signalArray[3])
+            self.xboxLabelClawState.setText("ClawState: "+signalArray[4])
+            self.xboxLabelRightMotor.setText("RightMotor: "+signalArray[5])
+            self.xboxLabelLeftMotor.setText("LeftMotor: "+signalArray[6])    
+            send_data(sigStr)    
 
     def stopXbox(self):
         self.xbox.stop()
 
-    def startServer(self):	         
-	if(self.server==None):  
-            self.server = customServer(port) 	
-	    self.connect( self.server,  self.server.signal, self.serverCallBackfunc)    
-	    self.server.start() 
+    def startServer(self):             
+        if(self.server==None):  
+            self.server = customServer(port)     
+            self.connect( self.server,  self.server.signal, self.serverCallBackfunc)    
+            self.server.start() 
 
     def serverCallBackfunc(self,sigStr):
-	if(sigStr == "" or sigStr is None):
-		return
+        if(sigStr == "" or sigStr is None):
+            return
         if(sigStr[0] == 'b'):
-	    if(sigStr[1] == '1'):
+            if(sigStr[1] == '1'):
                 self.bButton.setStyleSheet("background:green;color:black;")
             else:
                 self.bButton.setStyleSheet("background:white;color:black;")
         elif(sigStr[0] == 'T'):
-            
             globals.now = time.time()
             tokens = re.split('\s+', sigStr)
             sigStr = "Temperature: 55.8 C " + tokens[2]
             self.metaInformation.setText(sigStr)
-	elif(sigStr[0] == 'd'):
-            print(sigStr)
+        elif(sigStr[0] == 'd'):
+            #print(sigStr)
             dataValues = sigStr[1:].split(',')
-	    self.motorcurrent1.setText('Right Motor 1: ' + dataValues[0] + ' A')
+            self.motorcurrent1.setText('Right Motor 1: ' + dataValues[0] + ' A')
             self.motorcurrent2.setText('Right Motor 2: ' + dataValues[1] + ' A')
             self.motorcurrent3.setText('Left Motor 1: ' + dataValues[2] + ' A')
             self.motorcurrent4.setText('Left Motor 2: ' + dataValues[3] + ' A')
             self.pitch.setText('Pitch: ' + dataValues[4] + ' Degrees')
             self.roll.setText('ROll: ' + dataValues[5] + ' Degrees')
-            
+                
 
 
 
@@ -431,4 +454,4 @@ def main():
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    main()    
+    main()
